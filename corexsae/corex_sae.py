934d535fe794fa46b2f455a7d42d7621f41c366e
Rob Brekelman <brekelma@usc.edu>
2018-04-11 15:16:31 -0700

    a

--- corexsae/corex_sae.py
+++ corexsae/corex_sae.py
@@ -70,7 +70,7 @@ class CorexSAE(SupervisedLearnerPrimitiveBase[Input, Output, CorexSAE_Params, Co
 
 
 
-    def __init__(self, *, Hyperparams : CorexSAE_Hyperparams) -> None: #, random_seed : int =  0, docker_containers: typing.Dict[str, DockerContainer] = None
+    def __init__(self, *, hyperparams : CorexSAE_Hyperparams) -> None: #, random_seed : int =  0, docker_containers: typing.Dict[str, DockerContainer] = None
         super().__init__(hyperparams = Hyperparams) # random_seed = random_seed, docker_containers = docker_containers)
 
     def fit(self, *, timeout : float = None, iterations : int = None) -> CallResult[None]:
