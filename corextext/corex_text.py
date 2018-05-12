3250882b6bc7fb525d7e07947d5f226d1422a027
brekelma <brekelma@usc.edu>
2018-05-11 19:54:53 -0700

    Update corex_text.py

--- corextext/corex_text.py
+++ corextext/corex_text.py
@@ -71,7 +71,7 @@ class CorexText(UnsupervisedLearnerPrimitiveBase[Input, Output, CorexText_Params
             },
           "installation": [
                 {'type': 'PIP', 
-                 'package_uri': 'git+https://github.com/brekelma/dsbox_corex.git@'+str(utils.current_git_commit(os.path.dirname(__file__)))+'#egg=dsbox-corex'
+                 'package_uri': 'git+https://github.com/brekelma/dsbox_corex.git#egg=dsbox-corex'#@'+str(utils.current_git_commit(os.path.dirname(__file__)))+'#egg=dsbox-corex'
                 }
             ],
           "algorithm_types": ["EXPECTATION_MAXIMIZATION_ALGORITHM", "LATENT_DIRICHLET_ALLOCATION"],
