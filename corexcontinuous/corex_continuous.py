59d5cde615bbd1fd3faa6e539f75ac1db113a22e
brekelma <brekelma@usc.edu>
2018-05-11 18:57:33 -0700

    Update corex_continuous.py

--- corexcontinuous/corex_continuous.py
+++ corexcontinuous/corex_continuous.py
@@ -63,7 +63,7 @@ class CorexContinuous(UnsupervisedLearnerPrimitiveBase[Input, Output, CorexConti
       "installation": [
             {
              'type': 'PIP', 
-             'package_uri': 'git+https://github.com/brekelma/dsbox_corex.git@8672da14a7f2e00ea488da460ad68ef0799a9532#egg=dsbox-corex'
+             'package_uri': 'git+https://github.com/brekelma/dsbox_corex.git@'+str(utils.current_git_commit(os.path.dirname(__file__)))+'#egg=dsbox-corex'
             }
         ],
       "algorithm_types": ["EXPECTATION_MAXIMIZATION_ALGORITHM"],
