0f47e089f56eb6cf320ae9fb5fa65c5b01be263c
Serban Andrei Stan <serbanstan@Serbans-MBP.local>
2018-06-09 20:53:20 -0700

    update init

--- __init__.py
+++ __init__.py
@@ -2,5 +2,7 @@ from pkgutil import extend_path
 __path__ = extend_path(__path__, __name__)  
 from config import *
 import corex_text
+from corex_text import *
+
 import corex_continuous
 import corex_sae
